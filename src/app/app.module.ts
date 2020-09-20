0337ef7265d5b35ab4c7bc093323bb831814a657
AlexKlai92 <aklai@inbox.ru>
2020-09-20 10:48:48 +0300

    Добавлено (blog): post-item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { PostItemComponent } from './post-item/post-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    PostItemComponent
   ],
   imports: [
     BrowserModule,
